9e2a312b36592dbd21f59550df028fd7210fa43e
FinnTVD <finnit.th@gmail.com>
2024-03-06 18:14:06 +0700

    test

--- src/app/(main)/(account)/layout.jsx
+++ src/app/(main)/(account)/layout.jsx
@@ -1,10 +1,8 @@
-import InfoAccountPage from './@info/page'
-
-export default function AccountLayout({children}) {
+export default function AccountLayout({children, info}) {
   return (
     <main className='w-full h-fit bg-elevation-20 pt-[9.52rem] pb-[4.39rem]'>
       <div className='relative flex mx-auto w-fit h-fit'>
-        <InfoAccountPage />
+        {info}
         <div className='w-[50.87848rem] h-fit relative ml-[0.88rem]'>
           {children}
         </div>
