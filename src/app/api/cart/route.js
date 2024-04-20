b463edc1920c0c11993f2d230d49625e726f9eb2
Matt <huongax123@gmail.com>
2024-04-20 15:18:28 +0700

    update

--- src/app/api/cart/route.js
+++ src/app/api/cart/route.js
@@ -9,10 +9,22 @@ export async function getDataAuth(request) {
     redirect: 'follow',
   }
 
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API}${request.api}`,
-    requestOptions,
-  )
-  const data = await res.json()
-  return data
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API}${request.api}`,
+      requestOptions,
+    )
+
+    // Check if the response was successful
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`)
+    }
+
+    const data = await res.json()
+    return data
+  } catch (error) {
+    // Handle errors in fetching or data parsing
+    console.error('Failed to fetch data:', error.message)
+    throw error // Optionally re-throw the error if you want calling code to handle it
+  }
 }
